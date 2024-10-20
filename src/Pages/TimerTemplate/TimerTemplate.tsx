10eae0b308318d9dbe611dd08e0cfde64b2d24f9
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-10-20 22:37:41 +0200

    digital ready. Next text clock

--- src/Pages/TimerTemplate/TimerTemplate.tsx
+++ src/Pages/TimerTemplate/TimerTemplate.tsx
@@ -1,4 +1,5 @@
 import { Analog } from "./clock-components/Analog-clock/Analog";
+import { Digital } from "./clock-components/Digital-clock/Digital";
 import "./TimerTemplate.css";
 
 export function TimerTemplate() {
@@ -6,6 +7,7 @@ export function TimerTemplate() {
     <>
       <p className="Heading">interval</p>
       <Analog />
+      <Digital />
       <div className="button-container">
         <button type="button" className="abort-button">
           ABORT TIMER
---
+++ src/Pages/TimerTemplate/clock-components/Digital-clock/Digital.tsx
@@ -0,0 +1,11 @@
+import "./Digital.css";
+
+export function Digital() {
+  return (
+    <>
+      <div className="digital-clock-container">
+        <h1 className="digital-clock">7:32</h1>
+      </div>
+    </>
+  );
+}
